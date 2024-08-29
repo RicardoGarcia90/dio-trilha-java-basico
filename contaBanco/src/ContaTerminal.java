fc932d62b0576660fde3e784d34394cc551465b1
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2024-08-29 20:34:48 -0300

    Finalizando desafio

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite a agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.print("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + conta + " e seu saldo R$" + saldo + " já está disponível para saque.");
 
     }
 }
